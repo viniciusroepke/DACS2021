371b98f5a71b5d7e9dabecd50d9c2b4e3eb976a0
viniciusroepke <vinicius.roepke3@gmail.com>
2021-07-01 00:02:58 +0000

    primeira versao 1406

---
+++ src/main/java/br/univille/viniciusroepkedacs2021/controller/HomeController.java
@@ -0,0 +1,35 @@
+package br.univille.viniciusroepkedacs2021.controller;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    
+    @GetMapping
+    public ModelAndView index(){
+        
+        /*lógica doida*/
+        Date dataAgora = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
+        
+        HashMap<String, String> dados = new HashMap<>();
+        dados.put("tempo", sdf.format(dataAgora));
+        dados.put("nomeapp", "App Java lindo!!");
+        
+        return new ModelAndView("home/index", dados);
+    }
+
+
+    
+}
