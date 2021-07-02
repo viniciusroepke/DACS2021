b8a52d058d49c516dc2732a68798eafabede281f
viniciusroepke <vinicius.roepke3@gmail.com>
2021-07-02 01:23:50 +0000

    listagem de produtos - aula 2206

--- src/main/java/br/univille/viniciusroepkedacs2021/controller/HomeController.java
+++ src/main/java/br/univille/viniciusroepkedacs2021/controller/HomeController.java
@@ -4,6 +4,7 @@ package br.univille.viniciusroepkedacs2021.controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.TimeZone;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +21,13 @@ public class HomeController {
         
         /*lógica doida*/
         Date dataAgora = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+        SimpleDateFormat hora = new SimpleDateFormat("hh");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT-3:00"));
+        hora.setTimeZone(TimeZone.getTimeZone("GMT-3:00"));
         
         HashMap<String, String> dados = new HashMap<>();
-        dados.put("tempo", sdf.format(dataAgora));
+        dados.put("tempo", sdf.format(dataAgora)+", exatamente "+hora.format(dataAgora)+" horas e pouco");
         dados.put("nomeapp", "App Java lindo!!");
         
         return new ModelAndView("home/index", dados);
