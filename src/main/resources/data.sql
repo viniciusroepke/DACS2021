1a0c63ff3af2d32905373be1bcd68db4f7e065d6
viniciusroepke <vinicius.roepke3@gmail.com>
2021-11-29 22:20:53 +0000

    Commit dia 29-11

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT IGNORE INTO CATEGORIA(id, nome) values(1,'Brinquedos') ;
 INSERT IGNORE INTO CATEGORIA(id, nome) values(2,'Video Games') ;
 INSERT IGNORE INTO CATEGORIA(id, nome) values(3,'Jogos de Tabuleiro') ; 
+
+
+insert IGNORE into usuario(id,usuario,senha) values(1,'admin','admin');
